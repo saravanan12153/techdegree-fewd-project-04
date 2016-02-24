a38fd6ce0a20082bcb2659640f2354ecfa79d358
Fitri Ali <mail@fitri.co>
2016-02-24 16:27:30 +0800

    Rearranged and cleaned the codes

--- js/app.js
+++ js/app.js
@@ -8,19 +8,25 @@ $('#searchbox').show();
 
 function createLightbox(item) {
 	$('#lightbox').children().remove();
-	var lightboxLink = item.find('a').attr("href");
-	var lightboxTitle = item.find('img').attr("title");
-	var lightboxDescription = item.find('img').attr("alt");
-	var isExternal = lightboxLink.indexOf("http");
+	var lightboxLink = item.find('a').attr('href');
+	var lightboxTitle = item.find('img').attr('title');
+	var lightboxDescription = item.find('img').attr('alt');
+	var isExternal = lightboxLink.indexOf('http');
 	if (isExternal !== -1) {	
-		lightboxLink = lightboxLink.replace("watch?v=", "embed/");
-		$("#lightbox").append('<div id="lightbox-item"><iframe width="420" height="315" src="'+ lightboxLink + '?autoplay=1" frameborder="0" allowfullscreen></iframe><h2>' + lightboxTitle  + '</h2><p>' + lightboxDescription + '</p></div>');
+		lightboxLink = lightboxLink.replace('watch?v=', 'embed/');
+		$('#lightbox').append('<div id="lightbox-item"><iframe width="420" height="315" src="'+ lightboxLink + '?autoplay=1" frameborder="0" allowfullscreen></iframe><h2>' + lightboxTitle  + '</h2><p>' + lightboxDescription + '</p></div>');
 	}
 	else {
-		$("#lightbox").append("<div id='lightbox-item'><a href='" + lightboxLink + "'><img src='" + lightboxLink  + "'></a><h2>" + lightboxTitle  + "</h2><p>" + lightboxDescription + "</p></div>");
+		$('#lightbox').append('<div id="lightbox-item"><a href="' + lightboxLink + '"><img src="' + lightboxLink  + '"></a><h2>' + lightboxTitle  + '</h2><p>' + lightboxDescription + '</p></div>');
 	}
 	$('#lightbox, #lightbox-extra, #lightbox-left, #lightbox-right').show();
 }
+
+$(document).on('click', '.gallery-item',function(event) {
+	event.preventDefault();
+	$(this).addClass('selected');
+	createLightbox($('.selected'));
+});
 
 // Lightbox Navigation
 //---------------------------------------------
@@ -37,35 +43,7 @@ $('#lightbox-right').click( function(e) {
 	navigateright();
 });
 
-
-$('#lightbox-bw').click( function(e) {
-		$("#lightbox").toggleClass("desaturate");
-});
-
-function navigateleft() {
-	var $selected = $('.selected');
-	if($selected.is(':first-child')) {
-		$selected.removeClass("selected");
-		$(".gallery-item").last().addClass("selected");
-	} else {
-		$selected.removeClass("selected").prev().addClass("selected");
-	}
-	createLightbox($(".selected"));
-}
-
-function navigateright() {
-	var $selected = $('.selected');
-	if($selected.is(':last-child')) {
-		$selected.removeClass("selected");
-		$(".gallery-item").first().addClass("selected");
-	} else {
-		$selected.removeClass("selected").next().addClass("selected");
-	}
-	createLightbox($(".selected"));
-}
-
 function navigate(e) {
-	var $selected = $('.selected');
 	if(e.which == 39) {
 		navigateright();
 	}
@@ -74,47 +52,56 @@ function navigate(e) {
 	}
 }
 
-$("#lightbox").click( function(event) {
+function navigateleft() {
+	var $selected = $('.selected');
+	if($selected.is(':first-child')) {
+		$selected.removeClass('selected');
+		$('.gallery-item').last().addClass('selected');
+	} else {
+		$selected.removeClass('selected').prev().addClass('selected');
+	}
+	createLightbox($('.selected'));
+}
+
+function navigateright() {
+	var $selected = $('.selected');
+	if($selected.is(':last-child')) {
+		$selected.removeClass('selected');
+		$('.gallery-item').first().addClass('selected');
+	} else {
+		$selected.removeClass('selected').next().addClass('selected');
+	}
+	createLightbox($('.selected'));
+}
+
+// Desaturate / Black and White
+//---------------------------------------------
+
+$('#lightbox-bw').click( function(e) {
+		$('#lightbox').toggleClass('desaturate');
+});
+
+$('#lightbox').click( function(event) {
 	$('.selected').removeClass('selected');
 	$(this).children().remove();
 	$(this).hide();
 	$('#lightbox-extra, #lightbox-left, #lightbox-right').hide();
 });
 
-// Search Function
+// Search / Filter Function
 //---------------------------------------------
-
-/*
-var gallery = $('#gallery').html();
-
-$(document).on('keyup','#searchbox', function(e) {
-	var substring = $(this).val().toLowerCase();
-	var galleryItem;
-	var find;
-
-	$("#gallery").html(gallery);
-	$(".gallery-item").each( function() {
-		galleryItem = ($(this).find('img').attr('title') + " " + $(this).find('img').attr('alt')).toLowerCase();
-		find = galleryItem.indexOf(substring);
-		if (find === -1) {
-			$(this).fadeOut(200, function(){ $(this).detach()});
-		}
-	});
-});
-*/
 
 var removedItems = [];
 
 $(document).on('keyup','#searchbox', function(e) {
 	var substring = $(this).val().toLowerCase();
-	var galleryItem;
 	var find;
 
-	$(".gallery-item").each( function() {
+	$('.gallery-item').each( function() {
 		find = filter($(this), substring);
 		if (find === -1) {
 			removedItems.push($(this));
-			$(this).hide(500, function() {$(this).detach()});
+			$(this).hide(500, function() {$(this).detach();});
 		}
 	});
 	
@@ -126,18 +113,11 @@ $(document).on('keyup','#searchbox', function(e) {
 			removedItems.splice(i, 1);
 			i--;
 		}
-		console.log(removedItems.length);
 	}
 });
 
 function filter(string, substring) {
-		galleryItem = (string.find('img').attr('title') + " " + string.find('img').attr('alt')).toLowerCase();
-		return galleryItem.indexOf(substring);
+	var galleryItem = (string.find('img').attr('title') + " " + string.find('img').attr('alt')).toLowerCase();
+	return galleryItem.indexOf(substring);
 }
-
-$(document).on('click', '.gallery-item',function(event) {
-	event.preventDefault();
-	$(this).addClass('selected');
-	createLightbox($('.selected'));
-});
 
